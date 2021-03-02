08efd1e578eadd8e7218bd7ab37d3dc2cb17bfd4
ron <ronsalem557@gmail.com>
2021-03-02 20:09:50 +0200

    make it better

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/About.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
+    
 });
 
-app.listen(process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(port, () => console.log(`listening on port ${port}!`));
+
