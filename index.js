3fb10d694259157b7b25aef50e2ed7e322cd48a8
ron <ronsalem557@gmail.com>
2021-03-03 08:48:11 +0200

    test

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-// viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/about.html'));
     
