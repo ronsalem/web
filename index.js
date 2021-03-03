8a667a48b805db2c7d77e09dad375f099973c60e
ron <ronsalem557@gmail.com>
2021-03-03 08:51:43 +0200

    test2

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/about.html'));
-    
+app.get('/about', (req, res) => {
+    res.sendFile('/about.html', { root: __dirname });
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
