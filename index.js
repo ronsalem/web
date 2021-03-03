9c39a721600f772eb9526f76570cd3bd79691b74
ron <ronsalem557@gmail.com>
2021-03-03 13:00:23 +0200

    tesr

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3009;
 
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname + '/about.html'));
