292cf0916af8b1326c08cf03aab9f444adce26da
ron <ronsalem557@gmail.com>
2021-03-10 21:11:53 +0200

    update

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const path = require('path');
 const port = process.env.PORT || 3009;
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname + '/about.html'));
+    res.sendFile(path.join(__dirname + '/html files/about.html'));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/html files/home.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname + '/html files/about.html'));
+});
+
+app.get('/shop', (req, res) => {
+    res.sendFile(path.join(__dirname + '/html files/about.html'));
+});
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => console.log(`listening on port ${port}!`));
 
+const os = require('os')
+const uptime =  console.log(os.uptime());
