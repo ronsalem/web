598ae6c7f4df465d4f8789264c6bfafc47298435
ron <ronsalem557@gmail.com>
2021-03-10 21:19:51 +0200

    t

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const path = require('path');
 const port = process.env.PORT || 3009;
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname + './html files/about.html'));
+    res.sendFile(path.join(__dirname + './html/about.html'));
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + './html files/home.html'));
+    res.sendFile(path.join(__dirname + './html/home.html'));
 });
 
 app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname + './html files/about.html'));
+    res.sendFile(path.join(__dirname + './html/about.html'));
 });
 
 app.get('/shop', (req, res) => {
-    res.sendFile(path.join(__dirname + './html files/shop.html'));
+    res.sendFile(path.join(__dirname + './html/shop.html'));
 });
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => console.log(`listening on port ${port}!`));
